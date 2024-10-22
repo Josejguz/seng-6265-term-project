a63a5b05235793871e5b8f2087dd32789135c3cf
Maysun Dietrick <dietrickm16@students.ecu.edu>
2024-10-22 11:25:12 -0400

    created calculate_savings function

--- budget.py
+++ budget.py
@@ -14,3 +14,10 @@ class Budget:
     def add_expense(self, category, amount):
         self.expenses.append({'category': category, 'amount': amount})
         self.total_amount -= amount
+
+    def calculate_savings(self):
+        total_income = sum(item['amount'] for item in self.income)
+        total_expenses = sum(item['amount'] for item in self.expenses)
+        return total_income - total_expenses
+    
+    
